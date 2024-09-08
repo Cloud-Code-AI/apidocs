2eeca9e1bfcbe4caca4d73ae30cf2185adbc08e2
Saurav Panda <sgp65@cornell.edu>
2024-09-08 13:11:09 -0700

    added nginx and Dockerfile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import DocsGeneration from './pages/DocsPage/DocsGeneration'
 import './App.css'
 
+function Home() {
+  return <h1>Welcome to the Home Page</h1>
+}
+
 function App() {
   return (
-    <div className='flex w-full h-full'>
-      <DocsGeneration />
-    </div>
+    <Router>
+      <div className='flex flex-col w-full h-full'>
+        <nav>
+          <ul>
+            <li><Link to="/">Docs Generation</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<DocsGeneration />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
