d637901c1dbb7ee17f54e2ddc84737f757402490
Saurav Panda <sgp65@cornell.edu>
2024-09-08 13:32:41 -0700

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,20 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      external: ['vue']
+    }
+  },
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: [],
     },
   },
 })
